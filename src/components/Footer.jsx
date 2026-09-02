d5a3771985e40ec42006837c4202638f8c68edf4
Muhammad Hafiz <hafiz@axcell.my>
2026-09-02 21:48:11 +0800

    Update footer sitemap to include Certificates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -51,6 +51,7 @@ const Footer = () => {
           <ul>
             <li><Link to="/#about">About</Link></li>
             <li><Link to="/#experience">Experience</Link></li>
+            <li><Link to="/#certificates">Certificates</Link></li>
             <li><Link to="/#projects">Projects</Link></li>
             <li><Link to="/#contact">Contact</Link></li>
           </ul>
